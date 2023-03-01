78a3e0f06caf3e395f282a10de2945a9e1b1bcf0
erikahjung <erika.h.jung@gmail.com>
2023-02-28 19:54:34 -0800

    back-end framework using Node/Express/MongoDB with server successfully CRUD-ing

---
+++ server/bookModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {type: String, required: true},
+  author: {type: String, required: true},
+  status: {type: String, required: true}
+})
+const Book = mongoose.model('book', bookSchema);
+
+module.exports = Book;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,76 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require('path');
-const mongoose = require('mongoose');
 const PORT = 3000;
 
+const bookController = require('./bookController');
+
+//connect to the mongoDB
+const mongoose = require('mongoose');
+const MONGO_URI = 'mongodb+srv://erikahjung:mushroom@cluster0.eanhbnd.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(MONGO_URI, {
+  // options for the connect method to parse the URI
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // sets the name of the DB that our collections are part of
+  dbName: 'booklist'
+})
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
+
+//parse the application/json & the application/x-www-form-urlencoded incoming Request Object
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+//serve the client the html file and webpack bundle.js file
 // if (process.env.NODE_ENV === 'production') {
-  app.use('/build', express.static(path.join(__dirname, '../build')));
-  app.get('/', (req, res) => {
-    return res.status(200).sendFile(path.join(__dirname, '../index.html'))
-  })
+app.use('/build', express.static(path.join(__dirname, '../build')));
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'))
+})
 // }
 
-app.listen(PORT, () => {console.log('Listening on port 3000...')});
+const bookRouter = express.Router();
+app.use('/mybooklist', bookRouter);
+
+//GET request to get all the books from the db
+bookRouter.get('/', bookController.getBooks, (req, res) => {
+  return res.json(res.locals.books);
+})
+
+//POST request to add a book to the db
+bookRouter.post('/', bookController.addBook, (req, res) => {
+  return res.sendStatus(200);
+  // return res.status(200).json(res.locals.newBook);
+})
+
+//PATCH request to update a book in the db
+bookRouter.patch('/:id', bookController.updateBook, (req, res) => {
+  return res.sendStatus(200);
+  // return res.status(200).json(res.locals.updatedBook);
+})
+
+//DELETE request to delete a book in the db
+bookRouter.delete('/:id', bookController.deleteBook, (req, res) => {
+  return res.sendStatus(200);
+  // return res.status(200).json(res.locals.deletedBook);
+})
+
+//unknown route handler
+app.use((req, res) => {
+  return res.sendStatus(404);
+})
+
+//global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' }
+  }
+  const errorObj = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj.message);
+})
+
+//start the server on PORT
+app.listen(PORT, () => {console.log(`Listening on PORT: ${PORT}...`)});
